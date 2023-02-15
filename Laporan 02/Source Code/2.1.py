6619a51bdf4eec5420a4edaf0b9a2d431b879a6d
Akhmad Qasim <akhmadqasim@gmail.com>
2023-02-15 16:18:38 +0800

    Cicilan laporan 2

--- Laporan 02/Source Code/2.1.py
+++ Laporan 02/Source Code/2.1.py
@@ -1,14 +1,14 @@
 a = 12
 b = 37
 
-print(a + b * 4)
-print((a + b) * 4)
-print(a ** 10)
-print(a / 3)
-print(b / 3)
-print(a // b)
-print(a % b)
-print(a / b)
-print(a // b)
-print(a % b)
-print(b ** 100)
+print(a + b * 4)  # Data type: Integer
+print((a + b) * 4)  # Data type: Integer
+print(a ** 10)  # Data type: Integer
+print(a / 3)  # Data type: Float
+print(b / 3)  # Data type: Float
+print(a // b)  # Data type: Integer
+print(a % b)  # Data type: Integer
+print(a / b)  # Data type: Float
+print(a // b)  # Data type: Integer
+print(a % b)  # Data type: Integer
+print(b ** 100)  # Data type: Integer
--- Laporan 02/Source Code/2.2.py
+++ Laporan 02/Source Code/2.2.py
@@ -1,6 +1,11 @@
 a = 12
 b = 37
 
-print(a==b)
+print(a == b)
 print(b > a)
 print((b >= 1) and (b <= 10))
+
+print(3 != 4)
+print(1 == 1 and 2 < 3)
+print(1 > 10 or 2 < 3)
+print(not 1 > 10)
