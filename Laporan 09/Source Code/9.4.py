801e5ed7c8e14eb41fc1af8c76faa32a31d46486
Akhmad Qasim <akhmadqasim@gmail.com>
2023-04-09 10:56:01 +0800

    Source code 9 added

---
+++ Laporan 09/Source Code/9.4.py
@@ -0,0 +1,4 @@
+print("Nilai ordinal dari 'c' adalah", ord('c'))
+print("Nilai ordinal dari 'a' adalah", ord('a'))
+print("Nilai ordinal dari 'r' adalah", ord('r'))
+print("Nilai ordinal dari 'i' adalah", ord('i'))
